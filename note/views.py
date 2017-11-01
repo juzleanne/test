65103082106176e3d3e56560c1c1654406080d73
EvanEvanLeanneLeanne <EvanLeanne@evandeMacBook-Pro.local>
2017-11-01 22:20:00 +0000

    message

---
+++ note/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext, loader
+from django.http import HttpResponse
+from .models import Note
+ 
+# Create your views here.
+ 
+def home(request):
+    notes = Note.objects
+    template = loader.get_template('note.html')
+    context = {'notes': notes}
+    return render(request, 'note.html', context)
+    #return render_to_response("note.html", notes)
